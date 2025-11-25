3a8e62f949929bba079b5e263e14026df3695837
fionajoyamil <fionajoy.amil@neu.edu.ph>
2025-11-25 15:01:28 +0800

    Add Pyramid class to print pyramid pattern
    Implement a pyramid pattern based on user input.

---
+++ Pyramid.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pyramid 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter number for pyramid: ");
+        int number = input.nextInt();
+        
+        for (int x=number; x>=1; x--)
+        {
+            for (int y=1; y<= x; y++)
+            {
+               System.out.print(" ");
+            }
+            for (int z=1; z<=1; z++)
+            {
+                System.out.print("*\n");
+            }
+        }   
+    }
+}
